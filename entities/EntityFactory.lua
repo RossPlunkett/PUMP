a1f9033d51a6ae728ec9da108e39f7601ad6a34a
Ross <ross@rossplunkett.com>
2025-04-20 22:15:16 -0500

    working on LOVE 11.3 on mint Linux sans Revolver

--- entities/EntityFactory.lua
+++ entities/EntityFactory.lua
@@ -1,26 +1,20 @@
 local Entity = require("lib.Entity")
 local Vector2 = require("lib.Vector2")
 
-
 local EF = {}
 
 local Class = require("lib.Class")
 
-
-
-
 local ent_classes = {}
 
-ent_classes.ENTITY = require('classes.entities.ENTITY')
-ent_classes.HP = require('classes.entities.HP')
-ent_classes.CREATURE = require('classes.entities.CREATURE')
-ent_classes.GUN = require('classes.entities.GUN')
-ent_classes.PLAYER = require('classes.entities.PLAYER')
-ent_classes.PROJECTILE = require('classes.entities.PROJECTILE')
+ent_classes.ENTITY = require("classes.entities.ENTITY")
+ent_classes.HP = require("classes.entities.HP")
+ent_classes.CREATURE = require("classes.entities.CREATURE")
+ent_classes.GUN = require("classes.entities.GUN")
+ent_classes.PLAYER = require("classes.entities.PLAYER")
+ent_classes.PROJECTILE = require("classes.entities.PROJECTILE")
 
 local form_classes = {}
-
-
 
 local components = {} -- alphabetical
 
@@ -50,10 +44,9 @@ components.PlantZombie = require("entities.mobs.PlantZombie")
 components.Mom1 = require("entities.mobs.Mom1")
 components.Mom2 = require("entities.mobs.Mom2")
 
-
 -- gun forms
 components.Uzi = require("entities.guns.Uzi")
-components.Revolver = require("entities.guns.Revolver") -- doesn't exist yet
+-- components.Revolver = require("entities.guns.Revolver") -- doesn't exist yet
 components.PumpAction = require("entities.guns.PumpAction")
 components.Ak47 = require("entities.guns.Ak47")
 components.M16 = require("entities.guns.M16")
@@ -63,7 +56,6 @@ components.RustyPeacekeeper = require("entities.guns.RustyPeacekeeper")
 
 -- projectile forms
 components.MediumBullet = require("entities.projectiles.MediumBullet")
-
 
 -- atlases, alphbetical
 local atlases = {}
@@ -94,66 +86,59 @@ atlases.RustyPeacekeeper = love.graphics.newImage("assets/gfx/Weapons/Guns/Rusty
 -- projectile atlases
 atlases.MediumBullet = love.graphics.newImage("assets/gfx/Weapons/Guns/MediumBullet.png")
 
-
 -- runs run()
 _G.events:add("EF")
-_G.events:hook("EF", 
-    function(tbl) 
-        EF:run(tbl) 
-    end
-)
+_G.events:hook("EF", function(tbl)
+	EF:run(tbl)
+end)
 
 -- runs the :spawn() on the ent_name given on the form component
 _G.events:add("EF_spawn")
 _G.events:hook("EF_spawn", function(ent_name, ...)
-    components[ent_name]:spawn(...)
-end
-)
-
+	components[ent_name]:spawn(...)
+end)
 
 function EF:run(arg)
-    -- chooses class to base entity core on
-    local ent
-    if arg.ent_class then 
-        ent = ent_classes[arg.ent_class]()
-    else    
-        ent = Entity()
-    end
+	-- chooses class to base entity core on
+	local ent
+	if arg.ent_class then
+		ent = ent_classes[arg.ent_class]()
+	else
+		ent = Entity()
+	end
 
-    for i = 1, #arg do
-        local L = nil
-        if type(arg[i]) == "table" then
-            -- same thing here, specific removes and unpack() -- all this needs to be condensed into one table
-            L = table.remove(arg[i],1) -- L for label
-            local component = components[L](unpack(arg[i]))
-            -- .type property needed on components for naming on Entity
-            ent:add(component)
-        elseif type(arg[i]) == "string" then
-            L = arg[i]
-            -- .type property needed on components for naming on Entity
-            ent:add(components[L]())
-        end
-        --add sprite here
-        if L and components[L].create_sprite then
-            -- errors resulting from this line = components table and atlases table omissions, probably
-            local Sprite = components[L].create_sprite(atlases[L])
-            ent:add(Sprite)
-        end
-        
-    end
+	for i = 1, #arg do
+		local L = nil
+		if type(arg[i]) == "table" then
+			-- same thing here, specific removes and unpack() -- all this needs to be condensed into one table
+			L = table.remove(arg[i], 1) -- L for label
+			local component = components[L](unpack(arg[i]))
+			-- .type property needed on components for naming on Entity
+			ent:add(component)
+		elseif type(arg[i]) == "string" then
+			L = arg[i]
+			-- .type property needed on components for naming on Entity
+			ent:add(components[L]())
+		end
+		--add sprite here
+		if L and components[L].create_sprite then
+			-- errors resulting from this line = components table and atlases table omissions, probably
+			local Sprite = components[L].create_sprite(atlases[L])
+			ent:add(Sprite)
+		end
+	end
 
-    -- add Gizmo to every entity if enabled
-    if IsGizmoOn then ent:add(components["Gizmo"]()) end
+	-- add Gizmo to every entity if enabled
+	if IsGizmoOn then
+		ent:add(components["Gizmo"]())
+	end
 
-    ent:on_start() -- start it for initialization stuff
+	ent:on_start() -- start it for initialization stuff
 
-    _G.events:invoke("add to em", ent)
+	_G.events:invoke("add to em", ent)
 
-    return ent -- will this return values to the calling scope?
-
+	return ent -- will this return values to the calling scope?
 end
 
-
-
-
 return EF
+
--- scenes/Test.lua
+++ scenes/Test.lua
@@ -8,77 +8,69 @@ local world = require("lib.World")
 local World = world(32, 32, 50, 40)
 -- World:print()
 
-function T:new(scene_mgr) 
-    T.super.new(self, scene_mgr)
+function T:new(scene_mgr)
+	T.super.new(self, scene_mgr)
 
-    self.event_funcs = {}
-    
-    self.event_funcs.add_to_em = function(ent) self.em:add(ent) end
-    _G.events:add("add to em")
-    _G.events:hook("add to em", self.event_funcs.add_to_em)
+	self.event_funcs = {}
 
+	self.event_funcs.add_to_em = function(ent)
+		self.em:add(ent)
+	end
+	_G.events:add("add to em")
+	_G.events:hook("add to em", self.event_funcs.add_to_em)
 
-    --spawn players
-    _G.events:invoke("EF_spawn", "Cookie", {player_num = 2, x = 350, y = 380, control_scheme = "Gamepad"})
-    --_G.events:invoke("EF_spawn", "Candy", {player_num = 1, x = 350, y = 340, control_scheme = "Keyboard"})
-    -- _G.events:invoke("EF_spawn", "Cursor",  {x =0, y = 0,})
-    -- _G.events:invoke("EF_spawn", "cc2f7b", {player_num = 3, x = 320, y = 360})
-    -- _G.events:invoke("EF_spawn", "Flour", {player_num = 2, x = 390, y = 360})
-    -- _G.events:invoke("EF_spawn", "Vase", {player_num = 5, x = 490, y = 290})
+	--spawn players
+	_G.events:invoke("EF_spawn", "Cookie", { player_num = 2, x = 350, y = 380, control_scheme = "Keyboard" })
+	--_G.events:invoke("EF_spawn", "Candy", {player_num = 1, x = 350, y = 340, control_scheme = "Keyboard"})
+	-- _G.events:invoke("EF_spawn", "Cursor",  {x =0, y = 0,})
+	-- _G.events:invoke("EF_spawn", "cc2f7b", {player_num = 3, x = 320, y = 360})
+	-- _G.events:invoke("EF_spawn", "Flour", {player_num = 2, x = 390, y = 360})
+	-- _G.events:invoke("EF_spawn", "Vase", {player_num = 5, x = 490, y = 290})
 
-    _G.events:invoke("EF_spawn", "Mom1", {x=480, y=420})
-    _G.events:invoke("EF_spawn", "Mom2", {x=460, y=420})
+	_G.events:invoke("EF_spawn", "Mom1", { x = 480, y = 420 })
+	_G.events:invoke("EF_spawn", "Mom2", { x = 460, y = 420 })
 
-    -- EF is for Entity Factory          x    y
-    _G.events:invoke("EF_spawn", "Uzi", {x = 420, y = 400})
-    _G.events:invoke("EF_spawn", "Revolver", {x = 400, y = 420})
-    _G.events:invoke("EF_spawn", "PumpAction", {x = 400, y = 440})
-    _G.events:invoke("EF_spawn", "Ak47", {x = 400, y = 460})
-    _G.events:invoke("EF_spawn", "Ak47", {x = 400, y = 490})
-    _G.events:invoke("EF_spawn", "M16", {x = 440, y = 420})
-    _G.events:invoke("EF_spawn", "MagnumRevolver", {x = 440, y = 450})
-    _G.events:invoke("EF_spawn", "PopGun", {x = 360, y = 450})
-    _G.events:invoke("EF_spawn", "RustyPeacekeeper", {x = 460, y = 350})
+	-- EF is for Entity Factory          x    y
+	_G.events:invoke("EF_spawn", "Uzi", { x = 420, y = 400 })
+	--_G.events:invoke("EF_spawn", "Revolver", {x = 400, y = 420})
+	_G.events:invoke("EF_spawn", "PumpAction", { x = 400, y = 440 })
+	_G.events:invoke("EF_spawn", "Ak47", { x = 400, y = 460 })
+	_G.events:invoke("EF_spawn", "Ak47", { x = 400, y = 490 })
+	_G.events:invoke("EF_spawn", "M16", { x = 440, y = 420 })
+	_G.events:invoke("EF_spawn", "MagnumRevolver", { x = 440, y = 450 })
+	_G.events:invoke("EF_spawn", "PopGun", { x = 360, y = 450 })
+	_G.events:invoke("EF_spawn", "RustyPeacekeeper", { x = 460, y = 350 })
 
-    -- need to spawn a gun into a mob's hand:
-        -- 1: make the mob
-        -- 2: make the gun (perhaps the default gun is included in the mob somehow, like self.default_weapon)
-        -- 3: mob remotely picks up weapon
+	-- need to spawn a gun into a mob's hand:
+	-- 1: make the mob
+	-- 2: make the gun (perhaps the default gun is included in the mob somehow, like self.default_weapon)
+	-- 3: mob remotely picks up weapon
 
-    
-    print('my_obj')
-    print(my_obj)
+	print("my_obj")
+	print(my_obj)
 
-
-
-    self.alternate_frames = {
-        world_collision=2, -- meaning world collisions happen every x frames
-        world_collision_counter=0
-    }
-
-    
+	self.alternate_frames = {
+		world_collision = 2, -- meaning world collisions happen every x frames
+		world_collision_counter = 0,
+	}
 end
 
-
-
 function T:update(dt)
-    self.super.update(self,dt)
+	self.super.update(self, dt)
 
-    T.do_collisions(self)
-
+	T.do_collisions(self)
 end
 
 function T:draw()
-    love.graphics.clear(0.2588,0.5647,0.34509) -- stomach bg color
-    World:draw();
-    self.super.draw(self)
-    -- World:drawGrid()
-    love.graphics.print("entities: " .. #self.em.entities,Camera.x + 5,Camera.y + 25)
+	love.graphics.clear(0.2588, 0.5647, 0.34509) -- stomach bg color
+	World:draw()
+	self.super.draw(self)
+	-- World:drawGrid()
+	love.graphics.print("entities: " .. #self.em.entities, Camera.x + 5, Camera.y + 25)
 end
 
 function T:destroy()
-    _G.events:unhook("add to em", self.event_funcs.add_to_em)
+	_G.events:unhook("add to em", self.event_funcs.add_to_em)
 end
-
 
 return T
